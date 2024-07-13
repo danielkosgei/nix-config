fdf0d7657bd16c8930c9b7aa4b93e42ac8b41c68
enchanted-coder <danieltogey@pm.me>
2024-07-14 02:55:20 +0300

    fixed syntax error in kitty.nix

--- modules/home-manager/programs/misc/kitty.nix
+++ modules/home-manager/programs/misc/kitty.nix
@@ -12,7 +12,7 @@
 
     settings = {
       confirm_os_window_close = 0;
-      background_opacity = "1;  
+      background_opacity = "1";  
       window_padding_width = 10;
       scrollback_lines = 10000;
       enable_audio_bell = false;
